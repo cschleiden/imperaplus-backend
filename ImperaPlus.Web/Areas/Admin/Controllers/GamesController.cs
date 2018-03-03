c379081644599e411cba66743bf4744e4a20c930
cschleiden <cschleiden@live.de>
2018-03-03 11:22:26 -0800

    Some Games admin ui

---
+++ ImperaPlus.Web/Areas/Admin/Controllers/GamesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using DataTables.AspNet.AspNetCore;
+using DataTables.AspNet.Core;
+using ImperaPlus.Application.Games;
+using ImperaPlus.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImperaPlus.Backend.Areas.Admin.Controllers
+{
+    public class GamesController : BaseAdminController
+    {
+        private IGameService gameService;
+
+        public GamesController(IUnitOfWork unitOfWork, IGameService gameService)
+            : base(unitOfWork)
+        {
+            this.gameService = gameService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult View(long gameId)
+        {
+            var game = this.gameService.Get(gameId);
+
+            this.AddLookups();
+
+            return View(game);
+        }
+
+        [HttpPost]
+        public DataTablesJsonResult Data(IDataTablesRequest request)
+        {
+            var data = this.unitOfWork.Games.Query();
+
+            if (request.Search != null && !string.IsNullOrWhiteSpace(request.Search.Value))
+            {
+                long searchId = long.Parse(request.Search.Value);
+                data = data.Where(x => x.Id == searchId);
+            }
+
+            var dataPage = data
+                .OrderBy(x => x.Id)
+                .Skip(request.Start)
+                .Take(request.Length)
+                .Select(g => new
+                {
+                    g.Id,
+                    Type = g.Type.ToString(),                    
+                    g.Name,
+                    State = g.State.ToString(),
+                    PlayState = g.PlayState.ToString(),
+                    g.MapTemplateName,
+                    CreatedBy = g.CreatedBy.UserName,
+                    CurrentPlayer = "" // g.CurrentPlayer != null ? g.CurrentPlayer.User.UserName : ""
+                });
+
+            var response = DataTablesResponse.Create(request, data.Count(), data.Count(), dataPage);
+
+            return new DataTablesJsonResult(response, true);
+        }
+    }
+}
---
+++ ImperaPlus.Web/wwwroot/admin/scripts/games.js
@@ -0,0 +1,73 @@
+﻿$(document).ready(function () {
+    var table = $('#table-games').DataTable({
+        'responsive': true,
+        'processing': true,
+        'serverSide': true,
+        'ajax': {
+            'url': sourceLocation,
+            'type': 'POST'
+        },
+        'columns': [
+            {
+                "className": 'details-control',
+                "orderable": false,
+                "data": null,
+                "defaultContent": '<button>Expand</button>'
+            },
+            { 'data': "id" },
+            { 'data': "type" },
+            { 'data': "name" },
+            { 'data': "state" },
+            { 'data': "playState" },
+            { 'data': "mapTemplateName" },
+            { 'data': "createdBy" },
+            { 'data': "currentPlayer" },
+            {
+                'data': "id",
+                'render': function (data, type, row, meta) {
+                    return '<button onclick="deleteUser(\'' + data + '\')">Test</button>';
+                }
+            }
+        ],
+        'ordering': false,
+        'info': false,
+        'searchDelay': 1000
+    });
+
+    function deleteUser(id) {
+        /*$.ajax({
+        url: '@Url.Action("Delete", "Users")' + '?userId=' + id,
+        type: 'POST',
+        contentType: 'application/json',
+        success: function(result) {
+        alert("Removed");
+        }
+        });*/
+    }
+
+    function format(data) {
+        var $content = $("<div></div>");
+        $.get(viewLocation, { gameId: data.id }, result => {
+            $content.html(result);
+        });
+
+        return $content;
+    }
+
+    // Add event listener for opening and closing details
+    $('#table-games tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row(tr);
+
+        if (row.child.isShown()) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child(format(row.data())).show();
+            tr.addClass('shown');
+        }
+    });
+});
