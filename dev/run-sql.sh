26a96fdd0f3d893fd5e944203b7014ece276bb31
Christopher Schleiden <cschleiden@live.de>
2019-10-27 16:54:18 -0700

    Temp commit

--- dev/run-sql.sh
+++ dev/run-sql.sh
@@ -1,1 +1,1 @@
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=passw0rd1!' -p 1433:1433 –name sql2019_ctp32 -d mcr.microsoft.com/mssql/server:2019-CTP3.2-ubuntu
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=passw0rd1!' -p 1433:1433 –name sql2019_ctp32 -d 14eb731ca05b
