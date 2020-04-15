b59492e0d2189106acc2ec67dd60548fcb77815e
Christopher Schleiden <cschleiden@live.de>
2020-04-14 22:26:11 -0700

    Migration for V3 upgrade

---
+++ ImperaPlus.Web/Migrations/20200415052243_Prod.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImperaPlus.Web.Migrations
+{
+    public partial class Prod : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_AspNetUserLogins_LoginProvider_ProviderKey",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_AspNetUserLogins_LoginProvider_ProviderKey",
+                table: "AspNetUserLogins",
+                columns: new[] { "LoginProvider", "ProviderKey" });
+        }
+    }
+}
