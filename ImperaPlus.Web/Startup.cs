b061ece63a2792a3a2605ca214ab12da934f8833
Christopher Schleiden <cschleiden@live.de>
2020-04-15 19:32:28 -0700

    Cleanup dead code

--- ImperaPlus.Web/Startup.cs
+++ ImperaPlus.Web/Startup.cs
@@ -411,27 +411,6 @@ namespace ImperaPlus.Web
 
         public void ConfigureContainer(ContainerBuilder builder)
         {
-            // Register AutoMapper
-            // var assemblyNames = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
-            // var assembliesTypes = assemblyNames
-            //     .Where(a => a.Name.Contains("ImperaPlus", StringComparison.OrdinalIgnoreCase))
-            //     .SelectMany(an => Assembly.Load(an).GetTypes())
-            //     .Where(p => typeof(Profile).IsAssignableFrom(p) && p.IsPublic && !p.IsAbstract)
-            //     .Distinct();
-
-            // var autoMapperProfiles = assembliesTypes
-            //     .Select(p => (Profile)Activator.CreateInstance(p)).ToList();
-
-            // builder.Register(ctx => new MapperConfiguration(cfg =>
-            // {
-            //     foreach (var profile in autoMapperProfiles)
-            //     {
-            //         cfg.AddProfile(profile);
-            //     }
-            // }));
-
-            // builder.Register(ctx => ctx.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>().InstancePerLifetimeScope();
-
             // Messaging
             if (Environment.IsDevelopment())
             {
