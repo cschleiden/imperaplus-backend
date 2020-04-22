f1d358113dad027ecc7c45219571bf40ade89241
Christopher Schleiden <cschleiden@live.de>
2020-04-21 20:35:35 -0700

    Add some stats to the admin view

---
+++ ImperaPlus.Web/Areas/Admin/Controllers/StatsController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using ImperaPlus.Application.News;
+using ImperaPlus.Domain.Repositories;
+using ImperaPlus.Domain.Utilities;
+using ImperaPlus.DTO.News;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImperaPlus.Backend.Areas.Admin.Controllers
+{
+    public class StatsController : BaseAdminController
+    {
+        public StatsController(IUnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Logins60 = this.unitOfWork.Users.Query().Count(x => x.LastLogin >= DateTime.UtcNow.AddMinutes(-60));
+            ViewBag.Logins120 = this.unitOfWork.Users.Query().Count(x => x.LastLogin >= DateTime.UtcNow.AddMinutes(-120));
+            ViewBag.Games120 = this.unitOfWork.Games.Query().Count(x => x.LastTurnStartedAt >= DateTime.UtcNow.AddMinutes(-120));
+
+            return View();
+        }
+    }
+}
