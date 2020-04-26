03c5a37dffe4743b8a90e39716e539a5db21bcb9
Christopher Schleiden <cschleiden@live.de>
2020-04-25 20:47:03 -0700

    Clean up old tokens

--- ImperaPlus.Application/JobConfig.cs
+++ ImperaPlus.Application/JobConfig.cs
@@ -23,6 +23,7 @@ namespace ImperaPlus.Application
             // Cleanups
             RecurringJob.AddOrUpdate<UserCleanupJob>(UserCleanupJob.JobId, x => x.Handle(), Cron.Daily);
             RecurringJob.AddOrUpdate<GameCleanupJob>(GameCleanupJob.JobId, x => x.Handle(null), Cron.Hourly);
+            RecurringJob.AddOrUpdate<TokenCleanupJob>(TokenCleanupJob.JobId, x => x.Handle(null), Cron.Daily);
 
             // Manual
             RecurringJob.AddOrUpdate<LadderScorejob>(LadderScorejob.JobId, x => x.Handle(null), "0 0 31 2 0");
---
+++ ImperaPlus.Application/Jobs/TokenCleanup.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using Hangfire;
+using Hangfire.Server;
+using OpenIddict.Core;
+using OpenIddict.EntityFrameworkCore.Models;
+
+namespace ImperaPlus.Application.Jobs
+{
+    [Queue(JobQueues.Normal)]
+    [DisableConcurrentExecution(60)]
+    [AutomaticRetry(Attempts = 0)]
+    public class TokenCleanupJob : Job
+    {
+        public const string JobId = "TokenCleanup";
+
+        private OpenIddictTokenManager<OpenIddictToken> tokenManager;
+
+        private OpenIddictAuthorizationManager<OpenIddictAuthorization> authorizationManager;
+
+        public TokenCleanupJob(ILifetimeScope scope)
+            : base(scope)
+        {
+            this.tokenManager = this.LifetimeScope.Resolve<OpenIddictTokenManager<OpenIddictToken>>();
+            this.authorizationManager = this.LifetimeScope.Resolve<OpenIddictAuthorizationManager<OpenIddictAuthorization>>();
+        }
+
+        public override void Handle(PerformContext performContext)
+        {
+            base.Handle(performContext);
+
+            this.Log.Log(Domain.LogLevel.Info, "Cleaning up tokens...");
+
+            this.tokenManager.PruneAsync().RunSynchronously();
+
+            this.authorizationManager.PruneAsync().RunSynchronously();
+
+            this.Log.Log(Domain.LogLevel.Info, "Done");
+        }
+    }
+}
